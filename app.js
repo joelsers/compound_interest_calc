a14d535846320b7f6233860a7e76804197049a5c
joelsers <joeljacobs95@gmail.com>
2021-08-23 15:10:38 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let total = 0
+
+let monthlyInvestment = 1000
+
+// let percentIncrease = .00416
+
+let percentIncrease = .0082
+
+let months = 120
+
+let interest
+
+function compound() {
+    total += monthlyInvestment;
+    interest = total * percentIncrease
+    total += interest
+    console.log(total)
+    return total
+}
+
+for (let i=0; i< months; i++) {
+    compound()
+}
